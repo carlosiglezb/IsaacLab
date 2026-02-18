716b6737aa7b44af0ae3fac29dcaff518cb28d0d
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2026-02-18 00:22:16 -0600

    added knee knocker curriculum

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/mdp/curriculums.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/navigation/mdp/curriculums.py
@@ -41,10 +41,36 @@ def knee_knocker_levels_pose(
     # compute the distance in the x-direction that the robot walked
     final_pos_error = torch.norm(command[env_ids, :2], dim=1)
     # robots that walked far enough/pass door progress to harder terrains
-    move_up = final_pos_error < 0.3
+    move_up = final_pos_error < 0.1
     # robots that did not walk close enough to target go to simpler terrains
     move_down = ~move_up
     # update terrain levels
     door.update_env_origins(env_ids, move_up, move_down)
     # return the mean terrain level
     return torch.mean(door.terrain_levels.float())
+
+def terrain_levels_pose(
+    env: ManagerBasedRLEnv, env_ids: Sequence[int], asset_cfg: SceneEntityCfg = SceneEntityCfg("robot")
+): # -> torch.Tensor:
+    """Curriculum based on the distance the robot walked when commanded to move to a desired pose.
+
+    This term is used to increase the height of the knee knocker when the robot walks far enough and decrease the
+    difficulty when the robot walks less than half of the distance required by the commanded velocity.
+
+    Returns:
+        The mean terrain level for the given environment ids.
+    """
+    # extract the used quantities (to enable type-hinting)
+    asset: Articulation = env.scene[asset_cfg.name]
+    terrain: TerrainImporter = env.scene.terrain
+    command = env.command_manager.get_command("pose_command")
+    # compute the distance in the x-direction that the robot walked
+    final_pos_error = torch.norm(command[env_ids, :2], dim=1)
+    # robots that walked far enough/pass door progress to harder terrains
+    move_up = final_pos_error < 0.1
+    # robots that did not walk close enough to target go to simpler terrains
+    move_down = ~move_up
+    # update terrain levels
+    terrain.update_env_origins(env_ids, move_up, move_down)
+    # return the mean terrain level
+    return torch.mean(terrain.terrain_levels.float())
